ae3a9f78740fbe17e65bf14ec2cb4d777085647a
David Alejandro Gómez <david@acapillatech.com.ar>
2023-09-11 15:53:13 -0300

    Modificaciones en disposición de componentes y punto de colapsado

---
+++ global-templates/navbar-collapse-bootstrap5.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Header Navbar (bootstrap5)
+ *
+ * @package Understrap
+ * @since 1.1.0
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+?>
+
+<nav id="main-nav" class="navbar navbar-expand-xl navbar-dark bg-primary" aria-labelledby="main-nav-label">
+
+	<h2 id="main-nav-label" class="screen-reader-text">
+		<?php esc_html_e( 'Main Navigation', 'understrap' ); ?>
+	</h2>
+
+
+	<div class="container-fluid">
+
+		<!-- Your site branding in the menu -->
+		<?php get_template_part( 'global-templates/navbar-branding' ); ?>
+
+		<button
+			class="navbar-toggler"
+			type="button"
+			data-bs-toggle="collapse"
+			data-bs-target="#navbarNavDropdown"
+			aria-controls="navbarNavDropdown"
+			aria-expanded="false"
+			aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>"
+		>
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<!-- The WordPress Menu goes here -->
+		<?php
+		wp_nav_menu(
+			array(
+				'theme_location'  => 'primary',
+				'container_class' => 'collapse navbar-collapse',
+				'container_id'    => 'navbarNavDropdown',
+				'menu_class'      => 'navbar-nav ms-auto',
+				'fallback_cb'     => '',
+				'menu_id'         => 'main-menu',
+				'depth'           => 2,
+				'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
+			)
+		);
+		?>
+
+	</div><!-- .container(-fluid) -->
+
+</nav><!-- #main-nav -->
