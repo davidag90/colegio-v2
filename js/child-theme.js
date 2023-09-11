344de46a8bc95947dd5792474b4aebb908a60ddf
David Alejandro Gómez <david@acapillatech.com.ar>
2023-09-11 09:49:38 -0300

    Inicialización de theme

--- js/child-theme.js
+++ js/child-theme.js
@@ -1,6 +1,6 @@
 /*!
   * Understrap v1.2.0 (https://understrap.com)
-  * Copyright 2013-2022 The Understrap Authors (https://github.com/understrap/understrap/graphs/contributors)
+  * Copyright 2013-2023 The Understrap Authors (https://github.com/understrap/understrap/graphs/contributors)
   * Licensed under GPL-3.0 (undefined)
   */
 (function (global, factory) {
--- src/build/browser-sync.config.js
+++ src/build/browser-sync.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	"proxy": "localhost/",
+	"proxy": "colodontcba.local/",
 	"notify": false,
 	"files": ["./css/*.min.css", "./js/*.min.js", "./**/*.php"]
 };
