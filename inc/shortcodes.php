d2dc32cc74d4670ef65d5802d0aa89f18bcb47bd
David Alejandro Gómez <david@acapillatech.com.ar>
2024-02-02 02:17:28 -0300

    Cambios en layout de aranceles, val min cons y revistas

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -73,36 +73,36 @@ if (! function_exists('mostrar_aranceles_vigentes')) {
 
         $args = array(
             'post_type' => 'arancel',
-            'nopaging' => true
+            'nopaging' => true,
+            'date_query' => array(
+                'after' => '2020-09-01',
+                'inclusive' => true
+            )
         );
         
         $query = new WP_Query($args);
-        $c = 0;
         
         if ($query->have_posts()):
             echo '<div id="resumen-aranceles">';
                 while($query->have_posts()):
                     $query->the_post();
-                    $mes_arancel = get_field('mes');
-                    $ano_arancel = get_field('ano');
-                    $link = get_field('link_doc');
-                    $titulo = get_the_title();
                     
-                    if($c == 0):
-                        $bg = 'bg-success';
-                    else:
-                        $bg = 'bg-primary';
-                    endif;
+                    $titulo = get_the_title();
+                    $thumb = get_the_post_thumbnail();
+                    $link = get_field('link_doc');
 
-                    echo '<div class="arancel ' . $bg . ' text-light text-center p-3 rounded position-relative d-flex flex-column align-items-center justify-content-start">';
-                        echo '<div class="icono mb-2"><i class="fa-solid fa-file-lines fa-3x"></i></div>';
-                        echo '<a href="' . $link . '" class="link-light stretched-link text-decoration-none" target="_blank">' . $titulo . '</a>';
+                    // Ternarios para chequear el primer arancel y destacarlo
+                    echo '<div class="arancel border border-dark ' . ($c == 0 ? 'bg-primary text-light border-opacity-50' : 'text-bg-light border-opacity-25') . ' rounded p-3">'; 
+                        echo '<div class="thumb-arancel">' . $thumb . '</div>';
+                        echo '<div class="desc-arancel">';
+                            echo '<h3 class="titulo-arancel">' . $titulo . '</h3>';
+                            echo '<a class="btn ' . ($c == 0 ? 'btn-light' : 'btn-primary link-light') . '" href="' . $link . '" target="_blank">Ver aranceles</a>';
+                        echo '</div>'; // .desc-arancel
                     echo '</div>'; // .arancel
 
                     $c++;
                 endwhile;
-            echo '</div>'; // .d-flex#resumen-aranceles
-            
+            echo '</div>'; // #resumen-aranceles            
             wp_reset_postdata(); // Restablece los datos del post original
         else:
             echo '<p>No se encontró contenido de esta sección.</p>';
@@ -131,14 +131,29 @@ if (! function_exists('mostrar_valor_min_cons')) {
         $c = 0;
         
         if ($query->have_posts()):
-            echo '<div class="list-group list-group-flush">';
-                while($query->have_posts()):
-                    $query->the_post();
-
-                    $link = get_field('link_doc'); 
-                    $titulo = get_the_title();
-
+            while($query->have_posts()):
+                $query->the_post();
+                
+                $link = get_field('link_doc'); 
+                $titulo = get_the_title();
+                $thumb = get_the_post_thumbnail();
+                
+                if($c == 0):
+                    echo '<div class="ultimo-folleto mb-4">';
+                        echo '<div class="bg-dark bg-opacity-75 text-light rounded p-2 mb-2">';
+                            echo $thumb;
+                            echo '<h4 class="h5 mx-0 my-2">' . $titulo . '</h4>';
+                        echo '</div>';
+                        echo '<a class="btn btn-dark d-block" href="' . $link . '" target="_blank">Descargar para imprimir</a>';
+                    echo '</div>'; // .ultimo-folleto
+                else:
+                    if ($c == 1):
+                        echo '<div class="list-group list-group-flush">';
+                    endif;
                     echo '<a href="' . $link . '" class="list-group-item bg-transparent" target="_blank"><i class="fa-solid fa-file-lines me-2"></i>' . $titulo . '</a>';
+                endif;
+
+                $c++;
                 endwhile;
             echo '</div>'; // .list-group
             
@@ -369,7 +384,7 @@ if (! function_exists('mostrar_revistas')) {
         $c = 0;
         
         if ($query->have_posts()):
-            echo '<div id="resumen-revistas">';
+            echo '<div id="resumen-revistas" class="mb-5">';
                 while($query->have_posts()):
                     $query->the_post();
                     
@@ -382,15 +397,17 @@ if (! function_exists('mostrar_revistas')) {
                     $date = get_the_date('Y-m-d H:i:s');
                     $date_obj = new DateTime($date);
 
-                    $date_month = $date_obj->format('F');
+                    $date_month = date_i18n('F', $date_obj->getTimestamp());
+                    $date_month_ok = ucfirst($date_month);
+
                     $date_year = $date_obj->format('Y');
                     
                     // Ternarios para chequear el primer ejemplar y destacarlo
-                    echo '<div class="revista ' . ($c == 0 ? 'bg-primary text-light' : '') . ' text-center p-3 position-relative d-flex flex-row align-items-start justify-content-start">';
+                    echo '<div class="revista border border-dark ' . ($c == 0 ? 'bg-primary text-light border-opacity-50' : 'text-bg-light border-opacity-25') . ' rounded p-3">';
                         echo '<div class="thumb-revista">' . $thumb . '</div>';
                         echo '<div class="desc-revista">';
                             echo '<h3 class="titulo-revista">Edición N° ' . $edicion . '</h3>';
-                            echo '<p class="meta-revista text-muted">' . $date_month . ' ' . $date_year .'</p>';
+                            echo '<p class="meta-revista">' . $date_month_ok . ' ' . $date_year .'</p>';
                             echo '<a class="btn ' . ($c == 0 ? 'btn-light' : 'btn-primary link-light') . '" href="' . $link . '" target="_blank">Ver ejemplar</a>';
                         echo '</div>'; // .desc-revista
                     echo '</div>'; // .revista
