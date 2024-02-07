6f4ac8b844c728918096cfb269f3d6d0f2566de7
David Alejandro Gómez <david@acapillatech.com.ar>
2024-02-06 23:45:04 -0300

    Mejoras varias en layout de elementos

--- loop-templates/content.php
+++ loop-templates/content.php
@@ -9,14 +9,14 @@
 defined( 'ABSPATH' ) || exit;
 ?>
 <div class="col">
-	<div <?php post_class('card h-100 border-0'); ?> id="post-<?php the_ID(); ?>">
+	<div <?php post_class('card h-100 border-0 bg-light bg-opacity-50 overflow-hidden'); ?> id="post-<?php the_ID(); ?>">
 		<div class="card-img-top-wrap">
 			<?php echo get_the_post_thumbnail( $post->ID, 'large', array('class' => 'card-img-top h-100 w-auto', 'style' => 'max-width: none;') ); ?>
 		</div>
 		
 		<div class="card-body d-flex flex-column align-items-start">
 			<p class="text-muted mb-0"><small><?php echo get_the_date(); ?></small></p>
-			<?php the_title( sprintf( '<h2 class="card-title h4"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>'); ?>
+			<?php the_title( sprintf( '<h2 class="card-title h3"><a href="%s" rel="bookmark" class="text-decoration-none">', esc_url( get_permalink() ) ), '</a></h2>'); ?>
 
 			<p class="card-text"><?php echo get_the_excerpt(); ?>...</p>
 
