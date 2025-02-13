2ca012068fdf4240129c369f5ba9cbac44210827
davidag90 <info@dagdev.com.ar>
2025-02-13 18:03:36 -0300

    Feat: show "noticias" in "Pacientes" section

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -667,3 +667,65 @@ if (! function_exists('mostrar_links_interes')) {
 }
 
 add_shortcode('mostrar-links-interes', 'mostrar_links_interes');
+
+
+if (! function_exists('noticias_pacientes')) {
+    function noticias_pacientes()
+    {
+        ob_start();
+
+        $args = array(
+            'category_name' => 'pacientes',
+            'posts_per_page' => 8,
+        );
+
+        $query = new WP_Query($args);
+
+        if ($query->have_posts()):
+            echo '<div id="noticias-pacientes">';
+            echo '<section class="splide p-2" aria-label="Novedades">';
+            echo '<div class="splide__track">';
+            echo '<ul class="splide__list">';
+            while ($query->have_posts()) {
+                $query->the_post();
+
+                $permalink = get_the_permalink();
+                $title = get_the_title();
+                $thumbnail = get_the_post_thumbnail_url();
+                $date = get_the_date();
+                $excerpt = get_the_excerpt();
+
+                echo '<li class="splide__slide">';
+                echo '<div class="noticia-pacientes card h-100">';
+                echo '<div class="card-img-top-wrap">';
+                echo '<a href="' . $permalink . '"><img src="' . $thumbnail . '" class="card-img-top"></a>';
+                echo '</div>';
+
+                echo '<div class="card-body d-flex flex-column justify-content-start">';
+                echo '<div class="post-date text-muted">' . $date . '</div>';
+                echo '<h3 class="card-title h4"><a href="' . $permalink . '">' . $title . '</a></h3>';
+                echo '<p>' . $excerpt . '</p>';
+                echo '<div class="read-more mt-auto pt-3 text-end">';
+                echo '<a class="btn btn-primary link-light understrap-read-more-link" href="' . $permalink . '">Leer más <span class="screen-reader-text"> sobre ' . $title . ' </span><i class="fa-solid fa-arrow-right-long"></i></a>';
+                echo '</div>'; // .read-more
+                echo '</div>'; // .card-body
+                echo '</div>'; // .noticia-pacientes.card
+                echo '</li>'; // col-12 col-md-4 col-lg-3
+            }
+            echo '</ul>'; // .splide__list
+            echo '</div>'; //.splide__track
+            echo '</section>'; // .splide
+            echo '</div>'; // #noticias-pacientes
+
+            wp_reset_postdata(); // Restablece los datos del post original
+        else:
+            echo '<p>No se encontró contenido de esta sección.</p>';
+        endif;
+
+        $output = ob_get_clean();
+
+        return $output;
+    }
+}
+
+add_shortcode('noticias-pacientes', 'noticias_pacientes');
